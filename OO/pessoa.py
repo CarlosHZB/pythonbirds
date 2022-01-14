c81302d88b24f4cca347c50e4db2368930f66fb8
CarlosHZB <carlosziliolibraga@hotmail.com>
2022-01-14 14:23:57 -0300

    Criado atributos de dados

--- OO/pessoa.py
+++ OO/pessoa.py
@@ -1,9 +1,14 @@
 class Pessoa:
+    def __init__(self, nome=None, idade=35):
+        self.nome = nome
+        self.idade = idade
+
     def cumprimentar(self):
         return 'Olá'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa('Roger', 49)
+    print(p.nome, p.idade)
     print(Pessoa.cumprimentar(p))
     #Linha de cima e de baixo são a mesma coisa
     print(p.cumprimentar())
